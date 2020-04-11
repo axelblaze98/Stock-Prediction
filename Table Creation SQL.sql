fce2939b201125dcb9e5f2e147b50260980e211f
AxelBlaze98 <paarthbhandari98@gmail.com>
2020-04-11 13:54:14 +0530

    added all files

---
+++ Table Creation SQL.sql
@@ -0,0 +1,15 @@
+#create database major1db;
+use major1db;
+
+#create table stock_predicted(stkid varchar(20),stk_name varchar(20),stock_predictedcur_price float(20,5),pr_price float(20,5));
+
+insert into stock_predicted(stkid,stk_name) values(1,'APPL');
+insert into stock_predicted(stkid,stk_name) values(2,'AMZ');
+insert into stock_predicted(stkid,stk_name) values(3,'F');
+insert into stock_predicted(stkid,stk_name) values(4,'FB');
+insert into stock_predicted(stkid,stk_name) values(5,'GOOG');
+insert into stock_predicted(stkid,stk_name) values(6,'IBM');
+insert into stock_predicted(stkid,stk_name) values(7,'INTC');
+insert into stock_predicted(stkid,stk_name) values(8,'MCD');
+insert into stock_predicted(stkid,stk_name) values(9,'MSFT');
+insert into stock_predicted(stkid,stk_name) values(10,'ORCL');
